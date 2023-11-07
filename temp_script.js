5d42f7cd69e700c1fbe44f4878ce82a2b8ec2e1e
ronan.sangouard <ronan.sangouard@gmail.com>
2023-11-07 10:38:04 +0100

    Added several pages

---
+++ temp_script.js
@@ -0,0 +1,19 @@
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const articles = document.querySelectorAll("[data-page]")
+
+for (let i = 0; i < navigationLinks.length; i++) {
+    const navPrefix = navigationLinks[i].id.split('-')[0];
+    navigationLinks[i].addEventListener("click", () => {
+        for (let j= 0; j < navigationLinks.length; j ++) {
+            if (articles[j].id.split('-')[0] === navPrefix) {
+                navigationLinks[j].classList.add("active")
+                articles[j].classList.add("active");
+                window.scrollTo(0, 0);           
+            }
+            else{
+                navigationLinks[j].classList.remove("active")
+                articles[j].classList.remove("active");
+            }
+        }
+  });}
