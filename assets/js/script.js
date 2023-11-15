8e91ec0305811bc6593f0eed061527e6e785475d
ronan.sangouard <ronan.sangouard@gmail.com>
2023-11-15 19:40:50 +0100

    Click on name goes back to about

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,38 @@
 // page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const articles = document.querySelectorAll("[data-page]")
+const navigationLinks = Array.from(
+  document.querySelectorAll("[data-nav-link]")).sort(
+    (a, b) => {a.id.localeCompare(b.id)}
+  );
+const articles = Array.from(
+  document.querySelectorAll("[data-page]")).sort(
+    (a, b) => {a.id.localeCompare(b.id)}
+  );
 
 for (let i = 0; i < navigationLinks.length; i++) {
-    const navPrefix = navigationLinks[i].id.split('-')[0];
     navigationLinks[i].addEventListener("click", () => {
         for (let j= 0; j < navigationLinks.length; j ++) {
-            if (articles[j].id.split('-')[0] === navPrefix) {
-                articles[j].classList.add("active");
-                window.scrollTo(0, 0);           
-            }
-            else{
-                articles[j].classList.remove("active");
-            }
-            if (j === i) {
+            if (i === j) {
               navigationLinks[j].classList.add("active");
+              articles[j].classList.add("active");
+              window.scrollTo(0, 0);           
             }
             else {
               navigationLinks[j].classList.remove("active");
+              articles[j].classList.remove("active");
             }
         }
   });}
+
+const nameButton = document.getElementById("header-title");
+nameButton.addEventListener("click", () => {
+  // go back to about
+  for (let i = 1; i < navigationLinks.length; i++) {
+    navigationLinks[i].classList.remove("active");
+    articles[i].classList.remove("active");
+  };
+
+  navigationLinks[0].classList.add("active");
+  articles[0].classList.add("active");
+
+  window.scrollTo(0, 0);  
+})
